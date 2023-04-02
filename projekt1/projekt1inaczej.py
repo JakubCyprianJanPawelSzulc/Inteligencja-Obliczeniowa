9a91be1a8c19b5931025b61ec34b7edd37cf31c7
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-04-02 16:55:12 +0200

    projekt cos tam poprawiony

--- projekt1/projekt1inaczej.py
+++ projekt1/projekt1inaczej.py
@@ -4,51 +4,58 @@ import time
 from csv import writer
 import random
 
-
-# items1 = np.array([1,2,3,4,5])
-# items2 = np.array([1,1,1,5,5])
-# items3 = np.array([2,8,1,6,3])
+items1 = np.array([1,2,3,4,5])
+items2 = np.array([1,1,1,5,5])
+items3 = np.array([2,8,1,6,3])
 
 
 # items1 = np.array([1,9,2,8,3,7,4,6,5,5,4,6])
 # items2 = np.array([10,10,10,10,10,1,1,1,1,1,2,2])
 # items3 = np.array([8,2,9,1,7,3,6,4,5,5,4,6])
 
-items1 = np.array([1,2,3,4,5,6,7,8,9,10,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,10,10,5,4,1])
-items2 = np.array([10,10,10,10,10,10,10,10,10,10,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,10,10,5,4,1])
-items3 = np.array([8,2,9,1,7,3,6,4,5,5,4,6,8,2,9,1,7,3,6,4,5,5,4,6,8,2,9,1,7,3])
+# items1 = np.array([1,2,3,4,5,6,7,8,9,10,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,10,10,5,4,1])
+# items2 = np.array([10,10,10,10,10,10,10,10,10,10,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,10,10,5,4,1])
+# items3 = np.array([8,2,9,1,7,3,6,4,5,5,4,6,8,2,9,1,7,3,6,4,5,5,4,6,8,2,9,1,7,3])
+
+# items1 = items1.reshape(-1, 1)
 
 capacity = 10
 
-# gene_space = [1,2,3,4,5]
-# gene_space = [1,2,3,4,5,6,7,8,9,10,11,12]
-gene_space = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-
-num_genes=items1.size
-num_generations = 20000
+gene_space = [0,1]
+num_genes=items1.size*items1.size
+num_generations = 1000
 num_parents_mating = 100
-sol_per_pop = 300
-mutation_percent_genes = 12
-keep_parents = 15
-
+sol_per_pop = 200
+mutation_percent_genes = 10
 
 
 def fitness_function(solution, solution_idx):
-    bins=np.unique(solution)
-    num_bins = bins.size
+    bins_divided= [solution[i:i+5] for i in range(0, len(solution), 5)]
+    empty_bins=0
+    num_bins = len(bins_divided)
 
-    for i in range(num_bins):
+    for i in range(len(random_items)):
+        check = 0
+        for j in range(len(bins_divided)):
+            if bins_divided[j][i] == 1:
+                check+=1
+            if j==len(bins_divided[i])-1 and check == 0 or check > 1:
+                return -100
+
+    for i in range(len(bins_divided)):
         bin_sum = 0
-        for j in range(len(solution)):
-            if solution[j] == bins[i]:
+        is_all_zero = np.all((bins_divided[i] == 0))
+        if is_all_zero:
+            empty_bins+=1
+        for j in range(len(random_items)):
+            if bins_divided[i][j] == 1:
                 bin_sum += random_items[j]
         if bin_sum > capacity:
             return -100
     
-    return num_bins*-1
+    return (num_bins-empty_bins)*-1
 
-
-for i in range(100):
+for i in range (100):
     random_items = random.choice([items1, items2, items3])
     random_items = random_items.reshape(-1, 1)
 
@@ -60,8 +67,6 @@ for i in range(100):
                         num_genes=num_genes,
                         mutation_percent_genes=mutation_percent_genes,
                         gene_space=gene_space,
-                        parent_selection_type="sss",
-                        keep_parents=keep_parents,
                         
     )
 
@@ -69,17 +74,14 @@ for i in range(100):
     end=time.time()
 
     solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    if solution_fitness*-1 <= 15:
-        with open('zeit3.csv', 'a', newline='') as f_object:
+    if solution_fitness*-1 <= 2:
+        with open('zeit1inaczej.csv', 'a', newline='') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow([end-start, solution_fitness*-1])
             f_object.close()
         print("Najlepsze rozwiązanie: ", solution_fitness*-1)
     else:
-        with open('zeit3.csv', 'a', newline='') as f_object:
+        with open('zeit1inaczej.csv', 'a', newline='') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow([0, solution_fitness*-1])
             f_object.close()
-        # print("Najlepsze rozwiązanie: ", solution)
-        # print("Czas: ", end-start)
-    # ga_instance.plot_fitness()
