e85ce9c578698090f8e6a26fadf031a64ac0c8fc
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-05-06 14:22:43 +0200

    projekt2 (cos tam dziala)

---
+++ projekt2/main.py
@@ -0,0 +1,71 @@
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+
+df = pd.read_csv("crimedatapreprocessed1.csv")
+
+
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2137)
+
+# label_encoder = LabelEncoder()
+# X_train[:, 1] = label_encoder.fit_transform(X_train[:, 1])
+# X_test[:, 1] = label_encoder.transform(X_test[:, 1])
+
+#encode y value
+
+label_encoder = LabelEncoder()
+y_all = label_encoder.fit_transform(np.concatenate([y_train, y_test]))
+y_train = y_all[:len(y_train)]
+y_test = y_all[len(y_train):]
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+dtc_small = DecisionTreeClassifier(max_depth=3, random_state=2137)
+dtc_large = DecisionTreeClassifier(random_state=2137)
+gnb = GaussianNB()
+knn = KNeighborsClassifier(n_neighbors=5)
+mlp = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000, random_state=2137)
+
+dtc_small.fit(X_train, y_train)
+dtc_large.fit(X_train, y_train)
+gnb.fit(X_train, y_train)
+knn.fit(X_train, y_train)
+mlp.fit(X_train, y_train)
+
+y_pred_dtc_small = dtc_small.predict(X_test)
+y_pred_dtc_large = dtc_large.predict(X_test)
+y_pred_gnb = gnb.predict(X_test)
+y_pred_knn = knn.predict(X_test)
+y_pred_mlp = mlp.predict(X_test)
+
+cm_dtc_small = confusion_matrix(y_test, y_pred_dtc_small)
+cm_dtc_large = confusion_matrix(y_test, y_pred_dtc_large)
+cm_gnb = confusion_matrix(y_test, y_pred_gnb)
+cm_knn = confusion_matrix(y_test, y_pred_knn)
+cm_mlp = confusion_matrix(y_test, y_pred_mlp)
+
+print('Dokładność: {:.2f}%'.format(accuracy_score(y_test, y_pred_dtc_small)*100))
+print('Macierz błędu:\n', cm_dtc_small)
+print('Dokładność: {:.2f}%'.format(accuracy_score(y_test, y_pred_dtc_large)*100))
+print('Macierz błędu:\n', cm_dtc_large)
+print('Dokładność: {:.2f}%'.format(accuracy_score(y_test, y_pred_gnb)*100))
+print('Macierz błędu:\n', cm_gnb)
+print('Dokładność: {:.2f}%'.format(accuracy_score(y_test, y_pred_knn)*100))
+print('Macierz błędu:\n', cm_knn)
+print('Dokładność: {:.2f}%'.format(accuracy_score(y_test, y_pred_mlp)*100))
+print('Macierz błędu:\n', cm_mlp)
+
--- projekt2/preprocessing1.py
+++ projekt2/preprocessing1.py
@@ -4,5 +4,8 @@ import numpy as np
 data = pd.read_csv('crimedata.csv', sep=',', header=0)
 threshold = len(data) * 0.5
 data = data.dropna(thresh=threshold, axis=1)
+
+data = data.fillna(0)
+data = data.drop(data.columns[[0, 1]], axis=1)
 data.to_csv('crimedatapreprocessed1.csv', sep=',', index=False)
 
