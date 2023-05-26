797e544e722631ac718bc341a7e2bfc6ddff350c
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-05-26 14:04:14 +0200

    projekt3 preprocessing

---
+++ projekt3/preprocessing/preprocessing.py
@@ -0,0 +1,46 @@
+import re
+from nltk.tokenize import word_tokenize
+import nltk
+
+def preprocessTweet(tweet):
+    tweet = re.sub(r'^\d{4}-\d{2}-\d{2}, ', '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+    tweet = re.sub(r'@\w+', '', tweet)
+    tweet = re.sub(r'https?://\S+', '', tweet)
+    tweet = tweet.lower()
+    words = tweet.split()
+    words = [re.sub(r'(?<=\w)[^\s\w](?![^\s\w])', '', word) for word in words]
+    words = [re.sub(r'[|«»“”‘’,"–—…-]', '', word) for word in words]
+    words = [re.sub(r'\([^)]*\)', '', word) for word in words]
+    words = [re.sub(r'\.\.+$', '', word) for word in words]
+    words = [re.sub(r'\.', '', word) for word in words]
+    words = [re.sub(r'^[\(\[]', '', word) for word in words]
+    words = [re.sub(r'[\)\]]$', '', word) for word in words]
+    words = [word for word in words if word]
+    preprocessedTweet = ' '.join(words)
+
+    stop_words = list(nltk.corpus.stopwords.words('english'))
+    words = word_tokenize(preprocessedTweet)
+    words = [w for w in words if not w in stop_words]
+    preprocessedTweet = ' '.join(words)
+
+
+    return preprocessedTweet
+
+def saveToFile(filename, tweets):
+    with open (filename, "w", encoding="utf-8") as file:
+        for tweet in tweets:
+            file.write(tweet + "\n")
+
+def getFile(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        tweets = file.readlines()
+    return tweets
+
+def preprocessFile(filename):
+    tweets = getFile(filename)
+    preprocessedTweets = [preprocessTweet(tweet) for tweet in tweets]
+    return preprocessedTweets
+
+saveToFile("preprocessedIsrael.txt", preprocessFile("../download/tweetsIsrael.txt"))
+saveToFile("preprocessedPalestine.txt", preprocessFile("../download/tweetsPalestine.txt"))
