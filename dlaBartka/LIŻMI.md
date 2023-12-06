633cbbff3c8d8373fee67a8e528c3b5a987bd5d2
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-12-06 18:24:55 +0100

    Delete dlaBartka directory

--- dlaBartka/LIŻMI.md
+++
@@ -1,3 +0,0 @@
-zapalim i wypijim: https://www.youtube.com/watch?v=x1VOpM1_8mk
-
-zapalim i wypijim 2: https://www.youtube.com/watch?v=nAxfXztoWjY
--- dlaBartka/dlaBartekOrazJakub/src/Keycloak.js
+++
@@ -1,8 +0,0 @@
-import Keycloak from "keycloak-js";
-const keycloak = new Keycloak({
- url: "http://localhost:8080/",
- realm: "keycloak-react-auth",
- clientId: "React-auth",
-});
-
-export default keycloak;
